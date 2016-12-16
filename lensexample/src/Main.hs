bd47d8ca0b6141a3e41a9fefa51b2c291551ea9c
Darren Rhea <darren.rhea@gmail.com>
2016-12-15 20:38:32 -0600

    lens example

--- lensexample/src/Main.hs
+++ lensexample/src/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import Control.Lens
+-- https://github.com/ekmett/lens#lens-lenses-folds-and-traversals
 main :: IO ()
 main = do
-  putStrLn "hello lens"
+  putStrLn "hello len r"
+  let x = (3, "dog")
+  putStrLn $ show $ x ^. _1
+  putStrLn . show $ view _1 x
+  -- forall x y.  x ^. y == view y x
+
+  let nest = (("dog", "chicken"), "elephant")
+  putStrLn . show . view _2 $ view _1 nest
+
 
